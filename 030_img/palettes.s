317a092e55a17d2b02e5f6c216404b1ac63632a6
Yuma Ohgami <yuma@ohgami.jp>
2020-10-04 20:27:03 +0900

    [WIP] add 030_img

---
+++ 030_img/palettes.s
@@ -0,0 +1,71 @@
+PaletteBG:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x0000			/* 1: 0,0,0 */
+	dc.w	0x0000			/* 2: None */
+	dc.w	0x0000			/* 3: None */
+	dc.w	0x0000			/* 4: None */
+	dc.w	0x0000			/* 5: None */
+	dc.w	0x0000			/* 6: None */
+	dc.w	0x0000			/* 7: None */
+	dc.w	0x0000			/* 8: None */
+	dc.w	0x0000			/* 9: None */
+	dc.w	0x0000			/* A: None */
+	dc.w	0x0000			/* B: None */
+	dc.w	0x0000			/* C: None */
+	dc.w	0x0000			/* D: None */
+	dc.w	0x0000			/* E: None */
+	dc.w	0x0000			/* F: None */
+
+PalettePB:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x0A00			/* 1: 0,0,170 */
+	dc.w	0x0400			/* 2: 0,0,85 */
+	dc.w	0x0040			/* 3: 0,85,0 */
+	dc.w	0x0A40			/* 4: 0,85,170 */
+	dc.w	0x0E40			/* 5: 0,85,255 */
+	dc.w	0x0440			/* 6: 0,85,85 */
+	dc.w	0x00AA			/* 7: 170,170,0 */
+	dc.w	0x0AAA			/* 8: 170,170,170 */
+	dc.w	0x0EAA			/* 9: 170,170,255 */
+	dc.w	0x04AA			/* A: 170,170,85 */
+	dc.w	0x0AEA			/* B: 170,255,170 */
+	dc.w	0x0EEA			/* C: 170,255,255 */
+	dc.w	0x004A			/* D: 170,85,0 */
+	dc.w	0x0A4A			/* E: 170,85,170 */
+	dc.w	0x044A			/* F: 170,85,85 */
+
+PalettePA:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x00AE			/* 1: 255,170,0 */
+	dc.w	0x0AAE			/* 2: 255,170,170 */
+	dc.w	0x0EAE			/* 3: 255,170,255 */
+	dc.w	0x04AE			/* 4: 255,170,85 */
+	dc.w	0x00EE			/* 5: 255,255,0 */
+	dc.w	0x0AEE			/* 6: 255,255,170 */
+	dc.w	0x0EEE			/* 7: 255,255,255 */
+	dc.w	0x04EE			/* 8: 255,255,85 */
+	dc.w	0x0004			/* 9: 85,0,0 */
+	dc.w	0x0404			/* A: 85,0,85 */
+	dc.w	0x00A4			/* B: 85,170,0 */
+	dc.w	0x0AA4			/* C: 85,170,170 */
+	dc.w	0x0EA4			/* D: 85,170,255 */
+	dc.w	0x04A4			/* E: 85,170,85 */
+	dc.w	0x0044			/* F: 85,85,0 */
+
+PaletteWI:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x0A44			/* 1: 85,85,170 */
+	dc.w	0x0E44			/* 2: 85,85,255 */
+	dc.w	0x0444			/* 3: 85,85,85 */
+	dc.w	0x0000			/* 4: None */
+	dc.w	0x0000			/* 5: None */
+	dc.w	0x0000			/* 6: None */
+	dc.w	0x0000			/* 7: None */
+	dc.w	0x0000			/* 8: None */
+	dc.w	0x0000			/* 9: None */
+	dc.w	0x0000			/* A: None */
+	dc.w	0x0000			/* B: None */
+	dc.w	0x0000			/* C: None */
+	dc.w	0x0000			/* D: None */
+	dc.w	0x0000			/* E: None */
+	dc.w	0x0000			/* F: None */
