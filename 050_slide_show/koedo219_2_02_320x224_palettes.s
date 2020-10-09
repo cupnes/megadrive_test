a0b098a3f0495c3e817a17e02fb02c0ed7bc6877
Yuma Ohgami <yuma@ohgami.jp>
2020-10-09 14:15:13 +0900

    [WIP] 050_slide_show: edit

---
+++ 050_slide_show/koedo219_2_02_320x224_palettes.s
@@ -0,0 +1,53 @@
+K02PaletteBG:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x0000			/* 1: 000000 */
+	dc.w	0x0000			/* 2: None */
+	dc.w	0x0000			/* 3: None */
+	dc.w	0x0000			/* 4: None */
+	dc.w	0x0000			/* 5: None */
+	dc.w	0x0000			/* 6: None */
+	dc.w	0x0000			/* 7: None */
+	dc.w	0x0000			/* 8: None */
+	dc.w	0x0000			/* 9: None */
+	dc.w	0x0000			/* A: None */
+	dc.w	0x0000			/* B: None */
+	dc.w	0x0000			/* C: None */
+	dc.w	0x0000			/* D: None */
+	dc.w	0x0000			/* E: None */
+	dc.w	0x0000			/* F: None */
+
+K02PalettePB:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x0000			/* 1: 131313 */
+	dc.w	0x0082			/* 2: 249A14 */
+	dc.w	0x00A2			/* 3: 2BA71A */
+	dc.w	0x0282			/* 4: 2F9E20 */
+	dc.w	0x00A2			/* 5: 31B11E */
+	dc.w	0x02A2			/* 6: 36A727 */
+	dc.w	0x0222			/* 7: 393939 */
+	dc.w	0x02A2			/* 8: 39B727 */
+	dc.w	0x02A2			/* 9: 3DA72F */
+	dc.w	0x00C2			/* A: 3DC41E */
+	dc.w	0x02C2			/* B: 3EC620 */
+	dc.w	0x02C4			/* C: 42C12D */
+	dc.w	0x02A4			/* D: 44B333 */
+	dc.w	0x02A4			/* E: 47B837 */
+	dc.w	0x02C4			/* F: 4AC336 */
+
+K02PalettePA:
+	dc.w	0x0000			/* 0: Transparent */
+	dc.w	0x04A4			/* 1: 4EB341 */
+	dc.w	0x02C4			/* 2: 53C83D */
+	dc.w	0x04A4			/* 3: 57BA48 */
+	dc.w	0x04C4			/* 4: 5AC846 */
+	dc.w	0x04C4			/* 5: 5FCF47 */
+	dc.w	0x04A6			/* 6: 60BF4E */
+	dc.w	0x0666			/* 7: 656565 */
+	dc.w	0x04C6			/* 8: 65D04D */
+	dc.w	0x0888			/* 9: 9B9B9B */
+	dc.w	0x0CCC			/* A: D4D4D4 */
+	dc.w	0x0EEE			/* B: E4E4E4 */
+	dc.w	0x0EEE			/* C: FFFFFF */
+	dc.w	0x0000			/* D: None */
+	dc.w	0x0000			/* E: None */
+	dc.w	0x0000			/* F: None */
